97d3db986b1cc8e6566e32b34c9065c1a668fee3
anilkeshwani <anilkeshwani@hotmail.com>
2025-01-18 22:57:42 +0000

    Tighten up optimizer method/config

--- ssi/optimizer.py
+++ ssi/optimizer.py
@@ -9,14 +9,11 @@ def setup_optimizer(
     cfg: DictConfig,
     model: TransformerDecoder,
     optimzer_state_dict: dict[str, Any] | None = None,
-) -> Optimizer | None:
-    """
-    Set up the optimizer. This method also handles loading the optimizer state_dict, if specified.
-    """
+) -> AdamW:
     if cfg.optimizer_in_bwd:
         raise NotImplementedError
     else:
         optimizer = AdamW(model.parameters(), **cfg.optimizer)
-        if optimzer_state_dict:
+        if optimzer_state_dict is not None:
             optimizer.load_state_dict(optimzer_state_dict)
         return optimizer
