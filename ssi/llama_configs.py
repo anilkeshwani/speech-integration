21d8ffa9725e7c9ddd1071a26ca194bbf2590f76
anilkeshwani <anilkeshwani@hotmail.com>
2025-01-18 15:38:45 +0000

    Specify abstract dataclass for Llama 3.2 configs and use singletons per model

--- ssi/llama_configs.py
+++ ssi/llama_configs.py
@@ -1,31 +1,65 @@
-from dataclasses import dataclass
+from dataclasses import asdict, dataclass
 
 
 @dataclass
-class ConfigLlama3_2_1B:
-    vocab_size: int = 128_256
-    num_layers: int = 16
-    num_heads: int = 32
-    num_kv_heads: int = 8
-    embed_dim: int = 2048
-    max_seq_len: int = 131072
-    intermediate_dim: int = 8192
-    attn_dropout: float = 0.0
-    norm_eps: float = 1e-5
-    rope_base: int = 500_000
-    scale_factor: int = 32
+class ConfigLlama3_2:
+    vocab_size: int
+    num_layers: int
+    num_heads: int
+    num_kv_heads: int
+    embed_dim: int
+    max_seq_len: int
+    intermediate_dim: int
+    attn_dropout: float
+    norm_eps: float
+    rope_base: int
+    scale_factor: int
+    _n_dsus: int = 0
+
+    @property
+    def n_dsus(self) -> int:
+        return self._n_dsus
+
+    @n_dsus.setter
+    def n_dsus(self, n_dsus: int) -> None:
+        if not isinstance(n_dsus, int) or (n_dsus < 0):
+            raise ValueError("n_dsus must be a non-negative integer")
+        self._n_dsus = n_dsus
+    
+
+    @property
+    def parameters(self) -> dict:
+        _dict = asdict(self)
+        _dict.pop("n_dsus")
+        return _dict
 
 
-@dataclass
-class ConfigLlama3_2_3B:
-    vocab_size: int = 128_256
-    num_layers: int = 28
-    num_heads: int = 24
-    num_kv_heads: int = 8
-    embed_dim: int = 3072
-    max_seq_len: int = 131072
-    intermediate_dim: int = 8192
-    attn_dropout: float = 0.0
-    norm_eps: float = 1e-5
-    rope_base: int = 500_000
-    scale_factor: int = 32
+# Singletons for the configs
+
+configllama3_2_1b = ConfigLlama3_2(
+    vocab_size=128_256,
+    num_layers=16,
+    num_heads=32,
+    num_kv_heads=8,
+    embed_dim=2048,
+    max_seq_len=131072,
+    intermediate_dim=8192,
+    attn_dropout=0.0,
+    norm_eps=1e-5,
+    rope_base=500_000,
+    scale_factor=32,
+)
+
+configllama3_2_3b = ConfigLlama3_2(
+    vocab_size=128_256,
+    num_layers=28,
+    num_heads=24,
+    num_kv_heads=8,
+    embed_dim=3072,
+    max_seq_len=131072,
+    intermediate_dim=8192,
+    attn_dropout=0.0,
+    norm_eps=1e-5,
+    rope_base=500_000,
+    scale_factor=32,
+)
