76227f1276a8a30ce3f62a40a508fcea463fdcc0
anilkeshwani <anilkeshwani@hotmail.com>
2025-01-19 00:17:38 +0000

    ssi/train.py skeleton

---
+++ ssi/train.py
@@ -0,0 +1,86 @@
+import torch
+from omegaconf import DictConfig, OmegaConf
+from torch.optim import AdamW
+from torch.optim.lr_scheduler import LambdaLR, LRScheduler
+from torch.utils.data import DataLoader, Dataset, DistributedSampler
+from torchtune import config, modules, training
+from torchtune.datasets import PackedDataset
+from torchtune.models.llama3 import Llama3Tokenizer
+from torchtune.modules import TransformerDecoder
+from torchtune.modules.loss import CEWithChunkedOutputLoss
+from torchtune.training import get_dtype
+from torchtune.training.precision import PRECISION_STR_TO_DTYPE
+from torchtune.utils import batch_to_device, get_device
+from tqdm import tqdm
+
+from ssi.constants import MODEL_KEY, OPTIMIZER_KEY, SEED
+from ssi.data.sft import SFTDataset
+from ssi.model import setup_llama3_2_1b
+from ssi.optimizer import setup_optimizer
+from ssi.tokenizer import setup_llama3_tokenizer
+
+
+SUPPORTED_DTYPES = [torch.float32, torch.bfloat16]
+
+
+def validate_cfg(cfg: DictConfig) -> None:
+    if PRECISION_STR_TO_DTYPE.get(cfg.dtype) not in SUPPORTED_DTYPES:
+        raise ValueError(f"Unsupported dtype: {cfg.dtype}. Supported dtypes: {SUPPORTED_DTYPES}")
+
+
+def train(cfg: DictConfig) -> None:
+    training.set_seed(seed=cfg.seed)
+    device_default: torch.device = get_device(cfg.device)
+    dtype_default: torch.dtype = get_dtype(cfg.dtype)
+    model: TransformerDecoder = setup_llama3_2_1b(
+        cfg=cfg,
+        model_state_dict=ckpt_dict[MODEL_KEY],  # NOTE require model ckpt
+        dtype_default=dtype_default,
+        device_default=device_default,
+    )
+    model.to(device=device_default)
+    model.train()
+
+    tokenizer, special_tokens = setup_llama3_tokenizer(**cfg.tokenizer)
+
+    optimizer: AdamW = setup_optimizer(cfg.optimizer, model, ckpt_dict.get(OPTIMIZER_KEY))  # NOTE optim. ckpt optional
+
+    loss_fn = CEWithChunkedOutputLoss()
+
+    if cfg.compile:
+        training.compile_loss(loss_fn)
+
+    if isinstance(loss_fn, CEWithChunkedOutputLoss):
+        model.set_num_output_chunks(loss_fn.num_output_chunks)
+
+    lr_lambda = lambda epoch: 1.0
+    lr_scheduler: LRScheduler
+    sampler: DistributedSampler
+
+    dataset_train = SFTDataset(
+        source=cfg.data.source,
+        model_tokenizer=cfg.data.source,
+        inference=cfg.data.source,
+        filter_fn=cfg.data.source,
+        train_on_input=cfg.data.source,
+        column_map=cfg.data.source,
+        new_system_prompt=cfg.data.source,
+        image_dir=cfg.data.source,
+        split=cfg.data.split,
+    )
+    dataset_dev: SFTDataset
+
+    if cfg.data.packed:
+        if tokenizer.max_seq_len is None:
+            raise ValueError("PackedDataset requires a max_seq_len to be set on the tokenizer.")
+        return PackedDataset(ds, max_seq_len=tokenizer.max_seq_len)
+
+    if cfg.optimizer_in_bwd:
+        raise NotImplementedError
+    else:
+        optimizer.zero_grad()  # zero gradients before training
+
+    for epoch in range(cfg.epochs):
+        sampler.set_epoch(epoch)  # distinct seed each epoch
+        for i, batch in tqdm(enumerate(data)):
+            pass
