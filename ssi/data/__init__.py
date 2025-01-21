b8b3c968ef3b2cd3b380f182a81f36870a47e73e
anilkeshwani <anilkeshwani@hotmail.com>
2025-01-21 12:20:25 +0000

    Add dataset and setup_data function

---
+++ ssi/data/__init__.py
@@ -0,0 +1,63 @@
+import logging
+import os
+import sys
+from functools import partial
+
+from omegaconf import DictConfig, ListConfig
+from sardalign.config import LOG_DATEFMT, LOG_FORMAT, LOG_LEVEL
+from torch.utils.data import DataLoader, Dataset, DistributedSampler
+from torchtune.data import padded_collate_packed, padded_collate_sft
+from torchtune.datasets import PackedDataset
+from torchtune.models.llama3 import Llama3Tokenizer
+from torchtune.training import get_world_size_and_rank
+
+from ssi.data.sft import SFTDataset
+
+
+logging.basicConfig(
+    format=LOG_FORMAT,
+    datefmt=LOG_DATEFMT,
+    level=os.environ.get("LOGLEVEL", LOG_LEVEL).upper(),
+    stream=sys.stdout,
+)
+
+LOGGER = logging.getLogger(__name__)
+
+
+def setup_data(
+    self,
+    cfg_dataset: DictConfig,
+    model_tokenizer: Llama3Tokenizer,
+) -> tuple[DistributedSampler, DataLoader]:
+    world_size, rank = get_world_size_and_rank()
+    shuffle = cfg_dataset.pop("shuffle")
+    batch_size = cfg_dataset.pop("batch_size")
+    packed = cfg_dataset.pop("packed", False)
+
+    if isinstance(cfg_dataset, ListConfig):
+        raise NotImplementedError
+    else:
+        dataset = SFTDataset(**cfg_dataset, model_tokenizer=model_tokenizer)
+
+    if packed:
+        dataset = pack_dataset(dataset, model_tokenizer)
+
+    sampler = DistributedSampler(dataset, num_replicas=world_size, rank=rank, shuffle=shuffle, seed=0)
+    dataloader = DataLoader(
+        dataset=dataset,
+        batch_size=batch_size,
+        sampler=sampler,
+        drop_last=True,  # dropping last avoids shape issues with compile + flex attention
+        collate_fn=(
+            partial(padded_collate_sft, padding_idx=self._tokenizer.pad_id, ignore_idx=self._loss_fn.ignore_index)
+            if not packed
+            else padded_collate_packed
+        ),
+    )
+    return sampler, dataloader
+
+
+def pack_dataset(dataset: Dataset, tokenizer: Llama3Tokenizer) -> PackedDataset:
+    if tokenizer.max_seq_len is None:
+        raise ValueError("PackedDataset requires a max_seq_len to be set on the tokenizer.")
+    return PackedDataset(dataset, max_seq_len=tokenizer.max_seq_len)
