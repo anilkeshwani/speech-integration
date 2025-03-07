4719c94459c8f63ea9ae2ce1cd95d2a92fccde84
anilkeshwani <anilkeshwani@hotmail.com>
2025-03-07 16:59:04 +0000

    Add draft custom padded_collate_sft to ssi.data to allow passing through the additional keys

--- ssi/data/__init__.py
+++ ssi/data/__init__.py
@@ -2,15 +2,19 @@ import logging
 import os
 import sys
 from functools import partial
-from typing import Callable
+from typing import Any, Callable
 
+import torch
+import torch.nn.functional as F
 import torchtune.data
 from omegaconf import DictConfig, ListConfig, OmegaConf
 from sardalign.config import LOG_DATEFMT, LOG_FORMAT, LOG_LEVEL
+from torch import Tensor
+from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import DataLoader, Dataset, DistributedSampler
 from torchtune import config
 from torchtune.config._utils import _get_component_from_path
-from torchtune.data import padded_collate_packed, padded_collate_sft
+from torchtune.data import padded_collate_packed
 from torchtune.data._common import CROSS_ENTROPY_IGNORE_IDX
 from torchtune.datasets import PackedDataset, TextCompletionDataset
 from torchtune.models.llama3 import Llama3Tokenizer
@@ -38,6 +42,10 @@ def setup_text_completion_data(
 ) -> tuple[DataLoader, DistributedSampler]:
     if isinstance(cfg_dataset, ListConfig):
         raise NotImplementedError("Support for the shuffle parameter needs to be added to use ConcatDataset.")
+    if cfg_dataset.get("packed", False):
+        # Strictly this doesn't have to affect CPT since we dont' need to change the collate
+        # function (as for SFT) but raised for consistency / as a reminder
+        raise NotImplementedError("Need to add a custom collate function to handle the PACKED case - not implemented.")
     dataset = TextCompletionDataset(tokenizer=model_tokenizer, **cfg_dataset.dataset)
     if cfg_dataset.get("packed", False):
         dataset = pack_dataset(dataset, model_tokenizer, split_across_pack=cfg_dataset.get("split_across_pack", False))
@@ -67,7 +75,9 @@ def setup_sft_data(
     loss_fn: CEWithChunkedOutputLoss | None = None,
 ) -> tuple[DataLoader, DistributedSampler]:
     if isinstance(cfg_dataset, ListConfig):
-        raise NotImplementedError
+        raise NotImplementedError("Support for list of datasets not implemented")
+    if cfg_dataset.get("packed", False):
+        raise NotImplementedError("Need to add a custom collate function to handle the PACKED case - not implemented.")
     dataset = SFTDataset(model_tokenizer=model_tokenizer, **cfg_dataset.dataset)
     if cfg_dataset.get("packed", False):
         dataset = pack_dataset(dataset, model_tokenizer, split_across_pack=False)
@@ -88,6 +98,72 @@ def setup_sft_data(
         collate_fn=collate_fn,
     )
     return dataloader, sampler
+
+
+####################################################################################################
+# Collate function - custom to handle passing of additional_keys -> link samples to GT (e.g. ASR)
+####################################################################################################
+
+
+def padded_collate_sft(
+    batch: list[dict[str, Any]],  # NOTE list[dict[str, list[int]]] in torchtune.data._collate.padded_collate_sft
+    padding_idx: int = 0,
+    ignore_idx: int = CROSS_ENTROPY_IGNORE_IDX,
+    additional_keys: list[str] = [],
+) -> dict[str, Tensor] | dict[str, Any]:
+    """Pad a batch of sequences to the longest sequence length in the batch, and
+    convert integer lists to tensors.
+
+    Args:
+        batch (list[dict[str, list[int]]]): A list of dictionaries containing input, label pairs.
+        padding_idx (int): Padding index for input ids. Defaults to 0.
+        ignore_idx (int): Padding index for labels. Defaults to -100.
+        additional_keys (list[str]): Additional keys to collate. Useful for returning sample IDs.
+
+    Returns:
+        dict[str, torch.Tensor] | dict[str, Any]: Collated input and label tensors and optionally additional key fields.
+
+    Example:
+        >>> token_pairs = [
+        >>>    {"tokens": [1, 2, 3], "labels": [4, 5, 6]},
+        >>>    {"tokens": [7,], "labels": [10,]},
+        >>> ]
+        >>> collated = padded_collate(
+        >>>    batch=token_pairs,
+        >>>    padding_idx=padding_idx,
+        >>>    ignore_idx=ignore_idx,
+        >>> )
+        >>> collated["tokens"]
+        >>> tensor([[1, 2, 3], [7, 0, 0]])
+        >>> collated["labels"]
+        >>> tensor([[4, 5, 6], [10, -100, -100]])
+    """
+    input_ids = pad_sequence(
+        [torch.tensor(x["tokens"]) for x in batch],
+        batch_first=True,
+        padding_value=padding_idx,
+    )
+    labels = pad_sequence(
+        [torch.tensor(x["labels"]) for x in batch],
+        batch_first=True,
+        padding_value=ignore_idx,
+    )
+
+    input_ids_seq_len = input_ids.shape[-1]
+    labels_seq_len = labels.shape[-1]
+
+    # Hack to pad correctly and not use max_seq_len, which is costly
+    if input_ids_seq_len > labels_seq_len:
+        labels = F.pad(labels, (0, input_ids_seq_len - labels_seq_len), value=ignore_idx)
+    elif labels_seq_len > input_ids_seq_len:
+        input_ids = F.pad(
+            input_ids,
+            (0, labels_seq_len - input_ids_seq_len),
+            value=padding_idx,
+        )
+
+    additional_keys_dict = {key: [x[key] for x in batch] for key in additional_keys}
+    return {"tokens": input_ids.long(), "labels": labels.long()} | additional_keys_dict
 
 
 def pack_dataset(dataset: Dataset, tokenizer: Llama3Tokenizer, split_across_pack: bool = False) -> PackedDataset:
