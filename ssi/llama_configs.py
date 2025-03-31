6b018913d82cdf929f4b9f508f0ad12deddc588d
anilkeshwani <anilkeshwani@hotmail.com>
2025-03-31 15:44:53 +0000

    Flesh out support for modality switch tokens and provide Llama 3.2 config attributes to allow us to demarcate text and DSU tokens (we will log these separately during training)

--- ssi/llama_configs.py
+++ ssi/llama_configs.py
@@ -3,7 +3,9 @@ from dataclasses import asdict, dataclass
 
 @dataclass
 class ConfigLlama3_2:
-    _vocab_size_txt: int
+    # NOTE attributes are hidden to prevent being exposed by parameters property to llama3_2 function
+    _base_vocab_size_txt: int
+    _n_special_txt: int
     num_layers: int
     num_heads: int
     num_kv_heads: int
@@ -15,6 +17,7 @@ class ConfigLlama3_2:
     rope_base: int
     scale_factor: int
     _n_dsus: int = 0
+    _modality_tokens: bool = False
 
     @property
     def n_dsus(self) -> int:
@@ -27,18 +30,30 @@ class ConfigLlama3_2:
         self._n_dsus = n_dsus
 
     @property
+    def modality_tokens(self) -> bool:
+        return self._modality_tokens
+
+    @modality_tokens.setter
+    def modality_tokens(self, enable: bool) -> None:
+        if not isinstance(enable, bool):
+            raise ValueError("modality_tokens must be boolean")
+        self._modality_tokens = enable
+
+    @property
     def vocab_size(self) -> int:
-        return self._vocab_size_txt + self.n_dsus
+        return self._base_vocab_size_txt + self._n_special_txt + self.n_dsus + (2 * self._modality_tokens)
 
     @property
     def parameters(self) -> dict:
+        """Return (only) the parameters needed to initialise a model with torchtune.models.llama3_2.llama3_2"""
         return {"vocab_size": self.vocab_size} | {k: v for k, v in asdict(self).items() if not k.startswith("_")}
 
 
 # Singletons for the configs
 
 configllama3_2_1b = ConfigLlama3_2(
-    _vocab_size_txt=128_256,
+    _base_vocab_size_txt=128_000,
+    _n_special_txt=256,
     num_layers=16,
     num_heads=32,
     num_kv_heads=8,
@@ -52,7 +67,8 @@ configllama3_2_1b = ConfigLlama3_2(
 )
 
 configllama3_2_3b = ConfigLlama3_2(
-    _vocab_size_txt=128_256,
+    _base_vocab_size_txt=128_000,
+    _n_special_txt=256,
     num_layers=28,
     num_heads=24,
     num_kv_heads=8,
