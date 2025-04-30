7da05677e88beb77156bfbf8a99b7c4d713579ff
anilkeshwani <anilkeshwani@hotmail.com>
2025-04-30 12:08:45 +0000

    Add LLAMA_3_2_3B_BASE_DIR path

--- ssi/extend_llama3_2/constants.py
+++ ssi/extend_llama3_2/constants.py
@@ -9,6 +9,7 @@ HAFH_DIR = Path(os.environ.get("HAFH", "/mnt/scratch-artemis/anilkeshwani/"))
 TORCHTUNE_BASE_MODELS_DIR = HAFH_DIR / "models" / "base" / "torchtune"
 TORCHTUNE_EXTENDED_MODELS_DIR = HAFH_DIR / "models" / "extended" / "torchtune"
 LLAMA_3_2_1B_BASE_DIR = TORCHTUNE_BASE_MODELS_DIR / "Llama-3.2-1B"
+LLAMA_3_2_3B_BASE_DIR = TORCHTUNE_BASE_MODELS_DIR / "Llama-3.2-3B"
 
 # Tokenizer (tiktoken) and model (HF safetensors) path relative to Llama 3.2 directory (from tune download)
 LLAMA_3_2_TOKENIZER_RELPATH = Path("original", "tokenizer.model")
