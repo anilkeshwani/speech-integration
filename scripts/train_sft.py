b8fcc40f55bf67b179758ede89a875d64beca73d
anilkeshwani <anilkeshwani@hotmail.com>
2025-05-19 15:20:39 +0000

    Drop basicConfig logger configuration *even in scripts*
    We'll do this with Hydra - it's sufficient

--- scripts/train_sft.py
+++ scripts/train_sft.py
@@ -1,24 +1,8 @@
 #!/usr/bin/env python
 
-import logging
-import os
-import sys
-
 import hydra
-from sardalign.config import LOG_DATEFMT, LOG_FORMAT, LOG_LEVEL
 
 from ssi.train import train
-
-
-logging.basicConfig(
-    format=LOG_FORMAT,
-    datefmt=LOG_DATEFMT,
-    level=os.environ.get("LOG_LEVEL", LOG_LEVEL).upper(),
-    stream=sys.stdout,
-    force=True,
-)
-
-LOGGER = logging.getLogger(__name__)
 
 
 @hydra.main(config_path="../conf", config_name="sft", version_base=None)
