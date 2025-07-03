80aecaf3dbb0e28aa9cee06fb90e955934e17ec2
anilkeshwani <anilkeshwani@hotmail.com>
2025-07-03 14:12:50 +0000

    Add type: ignore for batch type in train loop

--- ssi/train.py
+++ ssi/train.py
@@ -167,7 +167,7 @@ def train(cfg: DictConfig) -> None:
     wandb_logger.log_config(cfg)  # log config after parameter resolution + overrides
     for epoch in range(epochs_run, n_epochs):
         sampler_train.set_epoch(epoch)  # distinct seed each epoch
-        for i, batch in tqdm(enumerate(data_train), total=batches_per_epoch):
+        for i, batch in tqdm(enumerate(data_train), total=batches_per_epoch):  # type: ignore
             batch_to_device(batch, DEVICE)  # in-place
             for tt, ttcnt in count_token_types(batch["tokens"], token_type_ranges, tokenizer.pad_id).items():
                 token_type_counts_total[tt] += ttcnt
