anilkeshwani/speech-integration

commit b05b53c787cbc91e95d432bc4276deefac092259

Modularise data configuration files

conf/data/cpt/mls-focalcodec.yaml
@@ -1,0 +1,4 @@
+defaults:
+  - _base_
+_source: anilkeshwani/mls-focalcodec  # TODO confirm HF repo ID
+_sampling_rate: 16000  # TODO confirm FocalCodec model sampling rate

conf/data/cpt/mls-hubert_large_ll60k-layer_22.yaml
@@ -1,50 +1,4 @@
-train:
-  dataset:
-    source: anilkeshwani/mls-hubert_large_ll60k-layer_22
-    split: train
-    sequence_type: interleaved
-    interleave_kwargs:
-      sampling_rate: 16000  # sampling rate of audio input data to HuBERT ("model sampling rate")
-      downsampling_ratio: 320  # downsampling ratio for HuBERT i.e. 320 samples -> 1 token
-      mean_seq_len_tokens: 39.43  # mean sequence length in tokens of the MLS en trainset stratified sample (25%)
-      binom_prob: 0.1  # fraction of sequence to make up subspans -> recall: binomial expectation = np
-    deduplicate: ${speech.deduplicate}
-    use_modality_tokens: ${speech.use_modality_tokens}
-    add_eos: true
-    # Optional parameters passed to TextCompletionDataset to specify non-standard dataset keys (i.e. columns HF dataset)
-    tokenized_key: null  # if null -> fall back to default key in sardalign.constants
-    alignment_start_time_key: null  # if null -> fall back to default key in sardalign.constants
-    alignment_end_time_key: null  # if null -> fall back to default key in sardalign.constants
-    speech_tokens_key: null  # if null -> fall back to default key in sardalign.constants
-  dataloader:
-    batch_size: 16
-    drop_last: true
-    num_workers: 0
-    persistent_workers: false
-  shuffle: true
-  packed: false  # TODO packing causes CPU RAM overflow for large datasets (e.g. MLS)
-dev:
-  dataset:
-    source: anilkeshwani/mls-hubert_large_ll60k-layer_22
-    split: validation
-    sequence_type: interleaved
-    interleave_kwargs:
-      sampling_rate: 16000  # sampling rate of audio input data to HuBERT ("model sampling rate")
-      downsampling_ratio: 320  # downsampling ratio for HuBERT i.e. 320 samples -> 1 token
-      mean_seq_len_tokens: 39.43  # mean sequence length in tokens of the MLS en trainset stratified sample (25%)
-      binom_prob: 0.1  # fraction of sequence to make up subspans -> recall: binomial expectation = np
-    deduplicate: ${speech.deduplicate}
-    use_modality_tokens: ${speech.use_modality_tokens}
-    add_eos: true
-    # Optional parameters passed to TextCompletionDataset to specify non-standard dataset keys (i.e. columns HF dataset)
-    tokenized_key: null  # if null -> fall back to default key in sardalign.constants
-    alignment_start_time_key: null  # if null -> fall back to default key in sardalign.constants
-    alignment_end_time_key: null  # if null -> fall back to default key in sardalign.constants
-    speech_tokens_key: null  # if null -> fall back to default key in sardalign.constants
-  dataloader:
-    batch_size: 16
-    drop_last: false
-    num_workers: 0
-    persistent_workers: false
-  shuffle: false
-  packed: false
+defaults:
+  - _base_
+_source: anilkeshwani/mls-hubert_large_ll60k-layer_22
+_sampling_rate: 16000  # HuBERT model sampling rate

conf/data/cpt/mls-mimi-srvq_0.yaml
@@ -1,4 +1,4 @@
 defaults:
   - _base_
-
 _source: anilkeshwani/mls-mimi-srvq_0
+_sampling_rate: 24000  # Mimi (Moshi) model sampling rate

conf/data/cpt/mls-speechtokenizer-rvq_0.yaml
@@ -1,50 +1,4 @@
-train:
-  dataset:
-    source: anilkeshwani/mls-speechtokenizer-rvq_0
-    split: train
-    sequence_type: interleaved
-    interleave_kwargs:
-      sampling_rate: 24000  # *model* sampling rate: sampling rate of audio input data to SpeechTokenizer model
-      downsampling_ratio: 320  # downsampling ratio for SpeechTokenizer i.e. 320 samples -> 1 token
-      mean_seq_len_tokens: 39.43  # mean sequence length in tokens of the MLS en trainset stratified sample (25%)
-      binom_prob: 0.1  # fraction of sequence to make up subspans -> recall: binomial expectation = np
-    deduplicate: ${speech.deduplicate}
-    use_modality_tokens: ${speech.use_modality_tokens}
-    add_eos: true
-    # Optional parameters passed to TextCompletionDataset to specify non-standard dataset keys (i.e. columns HF dataset)
-    tokenized_key: null  # if null -> fall back to default key in sardalign.constants
-    alignment_start_time_key: null  # if null -> fall back to default key in sardalign.constants
-    alignment_end_time_key: null  # if null -> fall back to default key in sardalign.constants
-    speech_tokens_key: null  # if null -> fall back to default key in sardalign.constants
-  dataloader:
-    batch_size: 16
-    drop_last: true
-    num_workers: 0
-    persistent_workers: false
-  shuffle: true
-  packed: false  # TODO packing causes CPU RAM overflow for large datasets (e.g. MLS)
-dev:
-  dataset:
-    source: anilkeshwani/mls-speechtokenizer-rvq_0
-    split: validation
-    sequence_type: interleaved
-    interleave_kwargs:
-      sampling_rate: 24000  # *model* sampling rate: sampling rate of audio input data to SpeechTokenizer model
-      downsampling_ratio: 320  # downsampling ratio for SpeechTokenizer i.e. 320 samples -> 1 token
-      mean_seq_len_tokens: 39.43  # mean sequence length in tokens of the MLS en trainset stratified sample (25%)
-      binom_prob: 0.1  # fraction of sequence to make up subspans -> recall: binomial expectation = np
-    deduplicate: ${speech.deduplicate}
-    use_modality_tokens: ${speech.use_modality_tokens}
-    add_eos: true
-    # Optional parameters passed to TextCompletionDataset to specify non-standard dataset keys (i.e. columns HF dataset)
-    tokenized_key: null  # if null -> fall back to default key in sardalign.constants
-    alignment_start_time_key: null  # if null -> fall back to default key in sardalign.constants
-    alignment_end_time_key: null  # if null -> fall back to default key in sardalign.constants
-    speech_tokens_key: null  # if null -> fall back to default key in sardalign.constants
-  dataloader:
-    batch_size: 16
-    drop_last: false
-    num_workers: 0
-    persistent_workers: false
-  shuffle: false
-  packed: false
+defaults:
+  - _base_
+_source: anilkeshwani/mls-speechtokenizer-rvq_0
+_sampling_rate: 24000  # SpeechTokenizer model sampling rate

conf/data/sft/mls-focalcodec.yaml
@@ -1,0 +1,4 @@
+defaults:
+  - _base_
+
+_source: anilkeshwani/mls-focalcodec  # TODO confirm HF repo ID

conf/data/sft/mls-hubert_large_ll60k-layer_22.yaml
@@ -1,75 +1,4 @@
-train:
-  dataset:  # SFTDataset
-    source: anilkeshwani/mls-hubert_large_ll60k-layer_22
-    # model_tokenizer taken from code
-    inference: false  # False for training, False for validation loss, toggled to True in code for validation inference
-    deduplicate: ${speech.deduplicate}
-    filter_fn: null
-    train_on_input: true  # set to true to prevent forgetting; will hyperparam. search over this
-    column_map:
-      input: speech_tokens
-      output: transcript
-    # yamllint disable-line rule:line-length
-    new_system_prompt: 'You will act as an automatic speech recognition (ASR) system. Transcribe the speech tokens into English text. '
-    image_dir: null
-    use_modality_tokens: ${speech.use_modality_tokens}
-    # SFTDataset kwargs passed to datasets.load_dataset
-    split: train
-  dataloader:
-    batch_size: 2
-    drop_last: true
-    num_workers: 0
-    persistent_workers: false
-  shuffle: true  # passed to DistributedSampler
-  packed: false  # passed to pack_dataset to create a PackedDataset
-dev:
-  dataset:  # SFTDataset
-    source: anilkeshwani/mls-hubert_large_ll60k-layer_22
-    # model_tokenizer taken from code
-    inference: false  # False for training, False for validation loss, toggled to True in code for validation inference
-    deduplicate: ${speech.deduplicate}
-    filter_fn: null
-    train_on_input: true  # set to true to prevent forgetting; will hyperparam. search over this
-    column_map:
-      input: speech_tokens
-      output: transcript
-    # yamllint disable-line rule:line-length
-    new_system_prompt: 'You will act as an automatic speech recognition (ASR) system. Transcribe the speech tokens into English text. '
-    image_dir: null
-    use_modality_tokens: ${speech.use_modality_tokens}
-    # SFTDataset kwargs passed to datasets.load_dataset
-    split: validation  # <- NOTE different from train
-  dataloader:
-    batch_size: 2
-    drop_last: false
-    num_workers: 0
-    persistent_workers: false
-  shuffle: false  # passed to DistributedSampler <- NOTE different from train
-  packed: false  # passed to pack_dataset to create a PackedDataset
-test:
-  dataset:  # SFTDataset
-    source: anilkeshwani/mls-hubert_large_ll60k-layer_22
-    # model_tokenizer taken from code
-    inference: true  # NOTE inference flag set to True for test dataset -> no GT transcript + no EOS token appended
-    deduplicate: ${speech.deduplicate}
-    filter_fn: null
-    train_on_input: true  # set to true to prevent forgetting; will hyperparam. search over this
-    column_map:
-      input: speech_tokens
-      output: transcript
-    # yamllint disable-line rule:line-length
-    new_system_prompt: 'You will act as an automatic speech recognition (ASR) system. Transcribe the speech tokens into English text. '
-    image_dir: null
-    use_modality_tokens: ${speech.use_modality_tokens}
-    # SFTDataset kwargs passed to datasets.load_dataset
-    split: test
-    #  # NOTE Add dataset.additional_keys if required; current WER computation relies on sample order -> ID not required
-    # additional_keys:
-    #   - ID
-  dataloader:
-    batch_size: 2
-    # NOTE drop_last not used - hard coded to False for testing
-    num_workers: 0
-    persistent_workers: false
-  # Note shuffle not used - hard coded to False for testing
-  packed: false  # passed to pack_dataset to create a PackedDataset
+defaults:
+  - _base_
+
+_source: anilkeshwani/mls-hubert_large_ll60k-layer_22

conf/data/sft/mls-speechtokenizer-rvq_0.yaml
@@ -1,75 +1,4 @@
-train:
-  dataset:  # SFTDataset
-    source: anilkeshwani/mls-speechtokenizer-rvq_0
-    # model_tokenizer taken from code
-    inference: false  # False for training, False for validation loss, toggled to True in code for validation inference
-    deduplicate: ${speech.deduplicate}
-    filter_fn: null
-    train_on_input: true  # set to true to prevent forgetting; will hyperparam. search over this
-    column_map:
-      input: speech_tokens
-      output: transcript
-    # yamllint disable-line rule:line-length
-    new_system_prompt: 'You will act as an automatic speech recognition (ASR) system. Transcribe the speech tokens into English text. '
-    image_dir: null
-    use_modality_tokens: ${speech.use_modality_tokens}
-    # SFTDataset kwargs passed to datasets.load_dataset
-    split: train
-  dataloader:
-    batch_size: 2
-    drop_last: true
-    num_workers: 0
-    persistent_workers: false
-  shuffle: true  # passed to DistributedSampler
-  packed: false  # passed to pack_dataset to create a PackedDataset
-dev:
-  dataset:  # SFTDataset
-    source: anilkeshwani/mls-speechtokenizer-rvq_0
-    # model_tokenizer taken from code
-    inference: false  # False for training, False for validation loss, toggled to True in code for validation inference
-    deduplicate: ${speech.deduplicate}
-    filter_fn: null
-    train_on_input: true  # set to true to prevent forgetting; will hyperparam. search over this
-    column_map:
-      input: speech_tokens
-      output: transcript
-    # yamllint disable-line rule:line-length
-    new_system_prompt: 'You will act as an automatic speech recognition (ASR) system. Transcribe the speech tokens into English text. '
-    image_dir: null
-    use_modality_tokens: ${speech.use_modality_tokens}
-    # SFTDataset kwargs passed to datasets.load_dataset
-    split: validation  # <- NOTE different from train
-  dataloader:
-    batch_size: 2
-    drop_last: false
-    num_workers: 0
-    persistent_workers: false
-  shuffle: false  # passed to DistributedSampler <- NOTE different from train
-  packed: false  # passed to pack_dataset to create a PackedDataset
-test:
-  dataset:  # SFTDataset
-    source: anilkeshwani/mls-speechtokenizer-rvq_0
-    # model_tokenizer taken from code
-    inference: true  # NOTE inference flag set to True for test dataset -> no GT transcript + no EOS token appended
-    deduplicate: ${speech.deduplicate}
-    filter_fn: null
-    train_on_input: true  # set to true to prevent forgetting; will hyperparam. search over this
-    column_map:
-      input: speech_tokens
-      output: transcript
-    # yamllint disable-line rule:line-length
-    new_system_prompt: 'You will act as an automatic speech recognition (ASR) system. Transcribe the speech tokens into English text. '
-    image_dir: null
-    use_modality_tokens: ${speech.use_modality_tokens}
-    # SFTDataset kwargs passed to datasets.load_dataset
-    split: test
-    #  # NOTE Add dataset.additional_keys if required; current WER computation relies on sample order -> ID not required
-    # additional_keys:
-    #   - ID
-  dataloader:
-    batch_size: 2
-    # NOTE drop_last not used - hard coded to False for testing
-    num_workers: 0
-    persistent_workers: false
-  # Note shuffle not used - hard coded to False for testing
-  packed: false  # passed to pack_dataset to create a PackedDataset
+defaults:
+  - _base_
+
+_source: anilkeshwani/mls-speechtokenizer-rvq_0

conf/data/cpt/_base_.yaml
@@ -1,0 +1,55 @@
+# Base CPT data config — tokenizer-specific configs inherit from this via `defaults: [_base_]`
+# Only `_source`, `_sampling_rate`, and optionally `_downsampling_ratio` need to be set per tokenizer.
+
+# Private keys — set by child configs, interpolated into train/dev below
+_source: ???
+_sampling_rate: ???
+_downsampling_ratio: 320  # samples -> 1 token; override per tokenizer if needed
+train:
+  dataset:
+    source: ${_source}
+    split: train
+    sequence_type: interleaved
+    interleave_kwargs:
+      sampling_rate: ${_sampling_rate}
+      downsampling_ratio: ${_downsampling_ratio}
+      mean_seq_len_tokens: 39.43  # mean seq len in tokens of the MLS en trainset stratified sample (25%)
+      binom_prob: 0.1  # fraction of sequence to make up subspans; binomial expectation = np
+    deduplicate: ${speech.deduplicate}
+    use_modality_tokens: ${speech.use_modality_tokens}
+    add_eos: true
+    tokenized_key: null  # null -> fall back to default key in sardalign.constants
+    alignment_start_time_key: null
+    alignment_end_time_key: null
+    speech_tokens_key: null
+  dataloader:
+    batch_size: 16
+    drop_last: true
+    num_workers: 0
+    persistent_workers: false
+  shuffle: true
+  packed: false  # TODO packing causes CPU RAM overflow for large datasets (e.g. MLS)
+dev:
+  dataset:
+    source: ${_source}
+    split: validation
+    sequence_type: interleaved
+    interleave_kwargs:
+      sampling_rate: ${_sampling_rate}
+      downsampling_ratio: ${_downsampling_ratio}
+      mean_seq_len_tokens: 39.43
+      binom_prob: 0.1
+    deduplicate: ${speech.deduplicate}
+    use_modality_tokens: ${speech.use_modality_tokens}
+    add_eos: true
+    tokenized_key: null
+    alignment_start_time_key: null
+    alignment_end_time_key: null
+    speech_tokens_key: null
+  dataloader:
+    batch_size: 16
+    drop_last: false
+    num_workers: 0
+    persistent_workers: false
+  shuffle: false
+  packed: false

commit 4cad4c9465fb46d4f85023bef3730bd9d05401ac

Fix broken Hydra data config modularization
The modularization in b05b53c introduced two bugs that prevented all
Hydra config composition from working:

1. defaults: [_base_] in data/sft/*.yaml resolved to data/_base_ (the
   config group root) instead of data/sft/_base_ (the file location),
   because Hydra resolves defaults relative to the config group, not
   the filesystem directory.

2. ${_source} interpolations in the base configs resolved from the
   OmegaConf root, not from the data node where _source was defined,
   causing InterpolationKeyError.

Fix: move base configs to conf/data/_sft_base.yaml and
conf/data/_cpt_base.yaml (at the config group root where Hydra can
find them), eliminate all cross-file interpolation variables (_source,
_sampling_rate, _downsampling_ratio), and have child configs override
source and interleave_kwargs directly per split. Hydra's mandatory ???
marker catches missing overrides at compose time.

Also fix test_trainer_gpu.py: add config_name, tokenizer.path, and
dynamic checkpoint filename discovery needed for hydra.compose().

conf/data/_cpt_base.yaml
@@ -1,27 +1,23 @@
-# Base CPT data config — tokenizer-specific configs inherit from this via `defaults: [_base_]`
-# Only `_source`, `_sampling_rate`, and optionally `_downsampling_ratio` need to be set per tokenizer.
+# Base CPT data config — tokenizer-specific configs inherit via `defaults: [_cpt_base]`
+# Child configs MUST override `source` and `interleave_kwargs` for each split (train/dev).
 
-# Private keys — set by child configs, interpolated into train/dev below
-_source: ???
-_sampling_rate: ???
-_downsampling_ratio: 320  # samples -> 1 token; override per tokenizer if needed
 train:
   dataset:
-    source: ${_source}
+    source: ???
     split: train
     sequence_type: interleaved
     interleave_kwargs:
-      sampling_rate: ${_sampling_rate}
-      downsampling_ratio: ${_downsampling_ratio}
+      sampling_rate: ???
+      downsampling_ratio: 320  # samples -> 1 token; override per tokenizer if needed
       mean_seq_len_tokens: 39.43  # mean seq len in tokens of the MLS en trainset stratified sample (25%)
       binom_prob: 0.1  # fraction of sequence to make up subspans; binomial expectation = np
     deduplicate: ${speech.deduplicate}
     use_modality_tokens: ${speech.use_modality_tokens}
     add_eos: true
+    n_samples: null  # if set, stream only the first N samples (avoids full dataset download)
     tokenized_key: null  # null -> fall back to default key in sardalign.constants
     alignment_start_time_key: null
     alignment_end_time_key: null
-    n_samples: null  # if set, stream only the first N samples (avoids full dataset download)
     speech_tokens_key: null
   dataloader:
     batch_size: 16
@@ -32,21 +28,21 @@ train:
   packed: false  # TODO packing causes CPU RAM overflow for large datasets (e.g. MLS)
 dev:
   dataset:
-    source: ${_source}
+    source: ???
     split: validation
     sequence_type: interleaved
     interleave_kwargs:
-      sampling_rate: ${_sampling_rate}
-      downsampling_ratio: ${_downsampling_ratio}
+      sampling_rate: ???
+      downsampling_ratio: 320
       mean_seq_len_tokens: 39.43
       binom_prob: 0.1
     deduplicate: ${speech.deduplicate}
     use_modality_tokens: ${speech.use_modality_tokens}
     add_eos: true
+    n_samples: null
     tokenized_key: null
     alignment_start_time_key: null
     alignment_end_time_key: null
-    n_samples: null
     speech_tokens_key: null
   dataloader:
     batch_size: 16

conf/data/_sft_base.yaml
@@ -1,12 +1,9 @@
-# Base SFT data config — tokenizer-specific configs inherit from this via `defaults: [_base_]`
-# Only `_source` needs to be set per tokenizer.
-
-# Private key — set by child configs, interpolated into train/dev/test below
-_source: ???
+# Base SFT data config — tokenizer-specific configs inherit via `defaults: [_sft_base]`
+# Child configs MUST override `source` for each split (train/dev/test).
 
 train:
   dataset:  # SFTDataset
-    source: ${_source}
+    source: ???
     inference: false
     deduplicate: ${speech.deduplicate}
     filter_fn: null
@@ -30,7 +27,7 @@ train:
 
 dev:
   dataset:  # SFTDataset
-    source: ${_source}
+    source: ???
     inference: false
     deduplicate: ${speech.deduplicate}
     filter_fn: null
@@ -54,7 +51,7 @@ dev:
 
 test:
   dataset:  # SFTDataset
-    source: ${_source}
+    source: ???
     inference: true  # no GT transcript + no EOS token appended
     deduplicate: ${speech.deduplicate}
     filter_fn: null

conf/data/cpt/mls-focalcodec.yaml
@@ -1,4 +1,13 @@
 defaults:
-  - _base_
-_source: anilkeshwani/mls-focalcodec  # TODO confirm HF repo ID
-_sampling_rate: 16000  # TODO confirm FocalCodec model sampling rate
+  - _cpt_base
+
+train:
+  dataset:
+    source: anilkeshwani/mls-focalcodec  # TODO confirm HF repo ID
+    interleave_kwargs:
+      sampling_rate: 16000  # TODO confirm FocalCodec model sampling rate
+dev:
+  dataset:
+    source: anilkeshwani/mls-focalcodec  # TODO confirm HF repo ID
+    interleave_kwargs:
+      sampling_rate: 16000

conf/data/cpt/mls-hubert_large_ll60k-layer_22.yaml
@@ -1,4 +1,13 @@
 defaults:
-  - _base_
-_source: anilkeshwani/mls-hubert_large_ll60k-layer_22
-_sampling_rate: 16000  # HuBERT model sampling rate
+  - _cpt_base
+
+train:
+  dataset:
+    source: anilkeshwani/mls-hubert_large_ll60k-layer_22
+    interleave_kwargs:
+      sampling_rate: 16000  # HuBERT model sampling rate
+dev:
+  dataset:
+    source: anilkeshwani/mls-hubert_large_ll60k-layer_22
+    interleave_kwargs:
+      sampling_rate: 16000

conf/data/cpt/mls-mimi-srvq_0.yaml
@@ -1,4 +1,13 @@
 defaults:
-  - _base_
-_source: anilkeshwani/mls-mimi-srvq_0
-_sampling_rate: 24000  # Mimi (Moshi) model sampling rate
+  - _cpt_base
+
+train:
+  dataset:
+    source: anilkeshwani/mls-mimi-srvq_0
+    interleave_kwargs:
+      sampling_rate: 24000  # Mimi (Moshi) model sampling rate
+dev:
+  dataset:
+    source: anilkeshwani/mls-mimi-srvq_0
+    interleave_kwargs:
+      sampling_rate: 24000

conf/data/cpt/mls-speechtokenizer-rvq_0.yaml
@@ -1,4 +1,13 @@
 defaults:
-  - _base_
-_source: anilkeshwani/mls-speechtokenizer-rvq_0
-_sampling_rate: 24000  # SpeechTokenizer model sampling rate
+  - _cpt_base
+
+train:
+  dataset:
+    source: anilkeshwani/mls-speechtokenizer-rvq_0
+    interleave_kwargs:
+      sampling_rate: 24000  # SpeechTokenizer model sampling rate
+dev:
+  dataset:
+    source: anilkeshwani/mls-speechtokenizer-rvq_0
+    interleave_kwargs:
+      sampling_rate: 24000

conf/data/sft/mls-focalcodec.yaml
@@ -1,4 +1,12 @@
 defaults:
-  - _base_
+  - _sft_base
 
-_source: anilkeshwani/mls-focalcodec  # TODO confirm HF repo ID
+train:
+  dataset:
+    source: anilkeshwani/mls-focalcodec  # TODO confirm HF repo ID
+dev:
+  dataset:
+    source: anilkeshwani/mls-focalcodec  # TODO confirm HF repo ID
+test:
+  dataset:
+    source: anilkeshwani/mls-focalcodec  # TODO confirm HF repo ID

conf/data/sft/mls-hubert_large_ll60k-layer_22.yaml
@@ -1,4 +1,12 @@
 defaults:
-  - _base_
+  - _sft_base
 
-_source: anilkeshwani/mls-hubert_large_ll60k-layer_22
+train:
+  dataset:
+    source: anilkeshwani/mls-hubert_large_ll60k-layer_22
+dev:
+  dataset:
+    source: anilkeshwani/mls-hubert_large_ll60k-layer_22
+test:
+  dataset:
+    source: anilkeshwani/mls-hubert_large_ll60k-layer_22

conf/data/sft/mls-mimi-srvq_0.yaml
@@ -1,4 +1,12 @@
 defaults:
-  - _base_
+  - _sft_base
 
-_source: anilkeshwani/mls-mimi-srvq_0
+train:
+  dataset:
+    source: anilkeshwani/mls-mimi-srvq_0
+dev:
+  dataset:
+    source: anilkeshwani/mls-mimi-srvq_0
+test:
+  dataset:
+    source: anilkeshwani/mls-mimi-srvq_0

conf/data/sft/mls-speechtokenizer-rvq_0.yaml
@@ -1,4 +1,12 @@
 defaults:
-  - _base_
+  - _sft_base
 
-_source: anilkeshwani/mls-speechtokenizer-rvq_0
+train:
+  dataset:
+    source: anilkeshwani/mls-speechtokenizer-rvq_0
+dev:
+  dataset:
+    source: anilkeshwani/mls-speechtokenizer-rvq_0
+test:
+  dataset:
+    source: anilkeshwani/mls-speechtokenizer-rvq_0

tests/test_equivalence_e2e.py
@@ -114,7 +114,7 @@ _enable_deterministic_cuda()
 def _compose_cfg(tmp_path: Path) -> DictConfig:
     """Build the full SFT config by merging YAML files the way Hydra would.
 
-    Loads common.yaml + training.yaml + sft.yaml + data/sft/_base_.yaml +
+    Loads common.yaml + training.yaml + sft.yaml + data/_sft_base.yaml +
     data/sft/mls-hubert…yaml, merges them in order, resolves interpolations,
     and applies test-specific overrides.
     """
@@ -129,8 +129,8 @@ def _compose_cfg(tmp_path: Path) -> DictConfig:
     # Layer 3: sft.yaml (SFT-specific)
     sft = OmegaConf.load(conf / "sft.yaml")
 
-    # Layer 4: data/sft/_base_.yaml (base data config)
-    data_base = OmegaConf.load(conf / "data" / "sft" / "_base_.yaml")
+    # Layer 4: data/_sft_base.yaml (base data config)
+    data_base = OmegaConf.load(conf / "data" / "_sft_base.yaml")
 
     # Layer 5: data/sft/mls-hubert…yaml (tokenizer-specific override)
     data_override = OmegaConf.load(conf / "data" / "sft" / "mls-hubert_large_ll60k-layer_22.yaml")

tests/test_equivalence_e2e_full.py
@@ -109,7 +109,7 @@ def _compose_cfg(output_dir: Path) -> DictConfig:
     common = OmegaConf.load(conf / "common.yaml")
     training_cfg = OmegaConf.load(conf / "training.yaml")
     sft = OmegaConf.load(conf / "sft.yaml")
-    data_base = OmegaConf.load(conf / "data" / "sft" / "_base_.yaml")
+    data_base = OmegaConf.load(conf / "data" / "_sft_base.yaml")
     data_override = OmegaConf.load(conf / "data" / "sft" / "mls-hubert_large_ll60k-layer_22.yaml")
     data_cfg = OmegaConf.merge(data_base, data_override)
     cfg = OmegaConf.merge(common, training_cfg, sft, {"data": data_cfg})

tests/test_trainer_gpu.py
@@ -38,13 +38,24 @@ os.environ.setdefault("HF_HOME", "/home/anilkeshwani/.cache/huggingface")
 # ---------------------------------------------------------------------------
 
 EXTENDED_MODEL_DIR = TORCHTUNE_EXTENDED_MODELS_DIR / "Llama-3.2-1B-5000-dsus"
-_LOCAL_EXTENDED = Path("/home/anilkeshwani/models/extended/Llama-3.2-1B-5000-dsus")
-if _LOCAL_EXTENDED.exists():
-    EXTENDED_MODEL_DIR = _LOCAL_EXTENDED
+_CANDIDATE_DIRS = [
+    Path("/home/anilkeshwani/models/extended/Llama-3.2-1B-5000-dsus"),
+    Path("/home/ubuntu/models/extended/Llama-3.2-1B-5000-dsus"),
+]
+for _d in _CANDIDATE_DIRS:
+    if _d.exists():
+        EXTENDED_MODEL_DIR = _d
+        break
 
 
 def _has_extended_model() -> bool:
     return EXTENDED_MODEL_DIR.exists() and (EXTENDED_MODEL_DIR / "model.safetensors.index.json").exists()
+
+
+def _get_checkpoint_files() -> list[str]:
+    """Discover the safetensors checkpoint file(s) in the extended model dir."""
+    st_files = sorted(EXTENDED_MODEL_DIR.glob("*.safetensors"))
+    return [f.name for f in st_files]
 
 
 # All tests in this module require GPU + extended model
@@ -84,7 +95,7 @@ def _compose_sft_cfg(tmp_path, max_steps=10, eval_steps=5, save_steps=10, grad_a
             overrides=[
                 "speech.n_dsus=5000",
                 f"checkpointer.checkpoint_dir={EXTENDED_MODEL_DIR}",
-                'checkpointer.checkpoint_files=["model-00001-of-00001.safetensors"]',
+                f"checkpointer.checkpoint_files={_get_checkpoint_files()}",
                 f"checkpointer.output_dir={tmp_path}/checkpoints",
                 "data=sft/mls-hubert_large_ll60k-layer_22",
                 f"max_steps={max_steps}",
@@ -94,9 +105,11 @@ def _compose_sft_cfg(tmp_path, max_steps=10, eval_steps=5, save_steps=10, grad_a
                 f"data.train.dataloader.batch_size={batch_size}",
                 f"data.dev.dataloader.batch_size={batch_size}",
                 "log_interval=1",
+                f"tokenizer.path={EXTENDED_MODEL_DIR / 'original' / 'tokenizer.model'}",
                 "wandb.project=test-speech-integration",
                 "wandb.entity=null",
                 "wandb.log_dir=/tmp/wandb_test",
+                "config_name=sft",  # hydra:job.config_name not available in compose()
             ],
         )
     return cfg